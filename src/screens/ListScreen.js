c290c33a40d766e24a504f326fc637b92c5f740d
Arif Hussain <hussain.arif33@gmail.com>
2020-12-25 18:34:19 -0500

    Horizontal scrolling and hiding scroll indicator

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -12,20 +12,25 @@ const ListScreen = () => {
         {name: 'Friend #7'},
         {name: 'Friend #8'},
         {name: 'Friend #9'}
-
     ]
 
     return (
         <FlatList
+            horizontal
+            showsHorizontalScrollIndicator={false}
             keyExtractor={(friend) => friend.name}
             data={friends} 
             renderItem={({ item }) => {
-                return <Text>{item.name}</Text>
+                return <Text style={styles.textStyle}>{item.name}</Text>
             }}
         />
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    textStyle: {
+        marginVertical: 50
+    }
+});
 
 export default ListScreen;
