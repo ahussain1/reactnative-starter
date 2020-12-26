d78cab8c98454826b1d9cb4bf5feaa701345a97d
Arif Hussain <hussain.arif33@gmail.com>
2020-12-25 19:10:47 -0500

    Navigatin to new screen from button clicks

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,20 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.textStyle}>Arif Hussain</Text>;
+const HomeScreen = (props) => {
+  return (
+        <View>
+          <Text style={styles.textStyle}>Arif Hussain</Text>
+          <Button
+              onPress={() => props.navigation.navigate('Components')}
+              title="Go to the components demo"
+          />
+          <Button
+            title="Go to list demo"
+            onPress={() => props.navigation.navigate('List')}
+            />
+        </View>
+      );
 };
 
 const styles = StyleSheet.create({
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,25 +3,24 @@ import {View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ListScreen = () => {
     const friends = [
-        {name: 'Friend #1'},
-        {name: 'Friend #2'},
-        {name: 'Friend #3'},
-        {name: 'Friend #4'},
-        {name: 'Friend #5'},
-        {name: 'Friend #6'},
-        {name: 'Friend #7'},
-        {name: 'Friend #8'},
-        {name: 'Friend #9'}
+        {name: 'Friend #1', age: '20'},
+        {name: 'Friend #2', age: '24'},
+        {name: 'Friend #3', age: '16'},
+        {name: 'Friend #4', age: '20'},
+        {name: 'Friend #5', age: '29'},
+        {name: 'Friend #6', age: '24'},
+        {name: 'Friend #7', age: '27'},
+        {name: 'Friend #8', age: '15'},
+        {name: 'Friend #9', age: '23'}
     ]
 
     return (
         <FlatList
-            horizontal
-            showsHorizontalScrollIndicator={false}
+            style={styles.border}
             keyExtractor={(friend) => friend.name}
             data={friends} 
             renderItem={({ item }) => {
-                return <Text style={styles.textStyle}>{item.name}</Text>
+                return <Text style={styles.textStyle}>{item.name} - {item.age}</Text>
             }}
         />
     );
@@ -29,7 +28,13 @@ const ListScreen = () => {
 
 const styles = StyleSheet.create({
     textStyle: {
-        marginVertical: 50
+        marginVertical: 50,
+    },
+    border: {
+        borderBottomColor: 'black',
+        borderBottomWidth: 1,
+        marginLeft: 5,
+        marginRight: 5
     }
 });
 
