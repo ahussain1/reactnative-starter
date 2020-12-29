9ee1769ed0383196898d9e119b931a99e13c2e16
Arif Hussain <hussain.arif33@gmail.com>
2020-12-28 21:11:53 -0500

    Reusable state updates

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -5,18 +5,23 @@ import ColorCounter from './ColorCounter';
 const COLOR_INCREMENT = 20;
 
 const SquaresScreen = () => {
-  const incrementor = 20;
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
 
   const setColor = (color, change) => {
-    if ( color === 'red') {
-      if (red + change > 255 || red + change < 0) {
+    switch (color) {
+      case 'red':
+        red + change > 255 || red + change < 0 ? null : setRed(red + change);
         return;
-      } else {
-        setRed(red + change)
-      }
+      case 'green':
+        green + change > 255 || green + change < 0 ? null : setGreen(green + change);
+        return;
+      case 'blue':
+        blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change);
+        return;
+      default:
+        return;
     }
   };
 
@@ -28,13 +33,13 @@ const SquaresScreen = () => {
         color="Red"
       />
       <ColorCounter
-        onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
-        onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
+        onIncrease={() => setColor('blue', COLOR_INCREMENT)}
+        onDecrease={() => setColor('blue', -1 * COLOR_INCREMENT)}
         color="Blue"
       />
       <ColorCounter
-        onIncrease={() => setGreen(green + COLOR_INCREMENT)}
-        onDecrease={() => setGreen(green - COLOR_INCREMENT )}
+        onIncrease={() => setColor('green', COLOR_INCREMENT)}
+        onDecrease={() => setColor('green', -1 * COLOR_INCREMENT )}
         color="Green"
       />
 
