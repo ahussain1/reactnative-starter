e753cd1bec90b2e7f02930fcac1bfa0d80f27860
Arif Hussain <hussain.arif33@gmail.com>
2021-01-03 12:59:40 -0500

    Exercise solution - changing counter screen to use reducers

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,25 +1,38 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 
+const reducer = (state, action) => {
+  //state === { count: number }
+  switch(action.type) {
+    case'increment':
+      return { ...state, count: state.count + action.payload };
+    case 'decrement':
+      return { ...state, count: state.count - action.payload };
+    default:
+      return state;
+  }
+};
+
 const CounterScreen = () => {
     // todo: fix this
-    const [counter, setCounter] = useState(0);
+    const [state, dispatch] = useReducer(reducer, {count: 0})
 
     return (
       <View>
           <Button
-              title="Increase" onPress={() => {
-              setCounter(counter + 1)
+              title="Increase"
+              onPress={() => {
+                dispatch({type: 'increment', payload: 10})
             }}
           />
           <Button
             title="Decrease"
-            onPress={() =>{
-              setCounter(counter - 1);
+            onPress={() => {
+              dispatch({type: 'decrement', payload: 1})
             }}
           />
-          <Text>Current Count: {counter}</Text>
+          <Text>Current Count: {state.count}</Text>
       </View>
     );
 };
